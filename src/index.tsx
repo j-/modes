be23d6260f964307d3855f07835bc8b025a51dd0
j- <j@skeoh.com>
2018-04-05 16:49:23 +1000

    Add Redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import rootReducer from './store';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './components/App';
+import { Provider as StoreProvider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+const store = createStore(rootReducer, composeWithDevTools());
+
 ReactDOM.render(
-	<App />,
+	<StoreProvider store={store}>
+		<App />
+	</StoreProvider>,
 	document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
