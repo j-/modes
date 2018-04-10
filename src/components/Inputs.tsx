ca0522c0a44cfb0c4a73b84d63d86d7de94fd2cb
j- <j@skeoh.com>
2018-04-10 17:41:55 +1000

    Show mode labels inside text box

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -7,15 +7,15 @@ import './Inputs.css';
 const Inputs: React.StatelessComponent = () => (
 	<div className="Inputs">
 		<div className="Inputs-section Inputs-section-octal">
-			<label htmlFor="Inputs-input-octal">Octal</label>
+			<h2><label htmlFor="Inputs-input-octal">Octal</label></h2>
 			<OctalModeInput id="Inputs-input-octal" />
 		</div>
 		<div className="Inputs-section Inputs-section-decimal">
-			<label htmlFor="Inputs-input-decimal">Decimal</label>
+			<h2><label htmlFor="Inputs-input-decimal">Decimal</label></h2>
 			<DecimalModeInput id="Inputs-input-decimal" />
 		</div>
 		<div className="Inputs-section Inputs-section-symbolic">
-			<label htmlFor="Inputs-input-symbolic">Symbolic</label>
+			<h2><label htmlFor="Inputs-input-symbolic">Symbolic</label></h2>
 			<SymbolicInput id="Inputs-input-symbolic" />
 		</div>
 	</div>
