3ef77489acc916d17c0dc3c0f47cdf1ab434e8e5
j- <j@skeoh.com>
2018-04-10 16:46:09 +1000

    Remove mode container component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Mode from '../containers/Mode';
 import FileType from '../containers/FileType';
 import Inputs from './Inputs';
 import PermissionsGrid from '../containers/PermissionsGrid';
@@ -9,7 +8,7 @@ import FlagsList from './FlagsList';
 const App: React.StatelessComponent = () => (
 	<div className="App">
 		<h1>Mode</h1>
-		<Mode /><br />
+
 		<FileType />
 
 		<br />
