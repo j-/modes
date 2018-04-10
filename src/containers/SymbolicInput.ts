b5391b548176afdc9a81ab4d268206d217fa0810
j- <j@skeoh.com>
2018-04-10 17:44:27 +1000

    Make symbolic input read-only for now

--- src/containers/SymbolicInput.ts
+++ src/containers/SymbolicInput.ts
@@ -1,15 +1,23 @@
 import { connect } from 'react-redux';
+import * as classNames from 'classnames';
 import TextInput from '../components/TextInput';
 import { ReducerState, getSymbolString } from '../store';
 
 interface StateProps {
 	value?: string | string[] | number;
 	type?: string;
+	className?: string;
+	readOnly?: boolean;
 }
 
 const mapStateToProps = (state: ReducerState): StateProps => ({
 	value: getSymbolString(state),
 	type: 'string',
+	className: classNames('ModeInput', {
+		'ModeInput--is-invalid': false,
+		'ModeInput--is-editing': false,
+	}),
+	readOnly: true,
 });
 
 export default connect<StateProps>(
