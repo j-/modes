075a1f03d68ca7fb7f413f70a2e31094e4d64406
j- <j@skeoh.com>
2018-04-11 17:49:19 +1000

    Handle empty input values

--- src/store/index.ts
+++ src/store/index.ts
@@ -46,7 +46,7 @@ const reducer: Reducer<ReducerState> = (state = DEFAULT_STATE, action) => {
 	if (isActionSetOctalValue(action)) {
 		const { value } = action.data;
 		const parsed = parseInt(value, 8);
-		const mode = isNaN(parsed) ? state.mode : parsed;
+		const mode = value === '' ? 0 : isNaN(parsed) ? state.mode : parsed;
 		return {
 			...state,
 			mode,
@@ -58,7 +58,7 @@ const reducer: Reducer<ReducerState> = (state = DEFAULT_STATE, action) => {
 	if (isActionSetDecimalValue(action)) {
 		const { value } = action.data;
 		const parsed = parseInt(value, 10);
-		const mode = isNaN(parsed) ? state.mode : parsed;
+		const mode = value === '' ? 0 : isNaN(parsed) ? state.mode : parsed;
 		return {
 			...state,
 			mode,
@@ -120,32 +120,36 @@ export const getOctalRawValue = (state: ReducerState): string | null => (
 	state.octalValue
 );
 
-export const getOctalInputValue = (state: ReducerState): string => (
-	getOctalRawValue(state) || getModeStringOctal(state)
-);
-
 export const isOctalInputEditing = (state: ReducerState): boolean => (
 	state.octalValue !== null
 );
 
+export const getOctalInputValue = (state: ReducerState): string => (
+	isOctalInputEditing(state) ?
+		getOctalRawValue(state) as string :
+		getModeStringOctal(state)
+);
+
 export const isOctalInputValid = (state: ReducerState): boolean => (
-	state.octalValue === null || !isNaN(parseInt(state.octalValue, 8))
+	state.octalValue === null || state.octalValue === '' || !isNaN(parseInt(state.octalValue, 8))
 );
 
 export const getDecimalRawValue = (state: ReducerState): string | null => (
 	state.decimalValue
 );
 
-export const getDecimalInputValue = (state: ReducerState): string => (
-	getDecimalRawValue(state) || getModeStringDecimal(state)
-);
-
 export const isDecimalInputEditing = (state: ReducerState): boolean => (
 	state.decimalValue !== null
 );
 
+export const getDecimalInputValue = (state: ReducerState): string => (
+	isDecimalInputEditing(state) ?
+		getDecimalRawValue(state) as string :
+		getModeStringDecimal(state)
+);
+
 export const isDecimalInputValid = (state: ReducerState): boolean => (
-	state.decimalValue === null || !isNaN(parseInt(state.decimalValue, 10))
+	state.decimalValue === null || state.decimalValue === '' || !isNaN(parseInt(state.decimalValue, 10))
 );
 
 export const isShowingFlags = (state: ReducerState): boolean => (
