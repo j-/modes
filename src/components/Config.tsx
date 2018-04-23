f9e860408d16e44c5efe172a2e26423d7df8c48b
j- <j@skeoh.com>
2018-04-23 14:49:31 +1000

    Style the config checkboxes

--- src/components/Config.tsx
+++ src/components/Config.tsx
@@ -1,17 +1,28 @@
 import * as React from 'react';
 import ShowFlagCheckbox from '../containers/ShowFlagCheckbox';
 import HighlightBitsCheckbox from '../containers/HighlightBitsCheckbox';
+import './Config.css';
 
 const Config: React.StatelessComponent = () => (
 	<div className="Config">
 		<label>
 			<ShowFlagCheckbox />
-			Show flags
-		</label><br />
+			<em>Show flags</em>
+			<p>
+				Shows the constant name of each flag or mask next to its
+				checkbox or radio. These constants come from Linux's {' '}
+				<a href="https://github.com/torvalds/linux/blob/master/include/uapi/linux/stat.h">stat.h</a>.
+			</p>
+		</label>
 
 		<label>
 			<HighlightBitsCheckbox />
-			Highlight bits
+			<em>Highlight bits</em>
+			<p>
+				Highlights other values that will be toggled on hover. Values
+				will be pale yellow if some bits are flipped and a full yellow
+				if all bits are flipped.
+			</p>
 		</label>
 	</div>
 );
