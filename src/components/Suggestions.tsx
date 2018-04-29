8aa3f9079066f6afb5da23f28d3cb4f203ca99c5
j- <j@skeoh.com>
2018-04-29 16:18:03 +1000

    Add keys to fragment children
    Fixes warning

--- src/components/Suggestions.tsx
+++ src/components/Suggestions.tsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 
 const Suggestions: React.StatelessComponent = () => (
 	<>
-		<datalist id="octal-suggestions">
+		<datalist key={8} id="octal-suggestions">
 			<option value="0777" />
 			<option value="0755" />
 			<option value="0700" />
 		</datalist>
 
-		<datalist id="decimal-suggestions">
+		<datalist key={10} id="decimal-suggestions">
 			<option value="511" />
 			<option value="493" />
 			<option value="448" />
